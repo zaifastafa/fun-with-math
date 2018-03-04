5796202b607df03772cf99105017b6a6ad2a7d76
Huzaifa Mustafa <huzaifah.mustafa@gmail.com>
2018-03-04 14:10:57 +0500

    - fun with math game

---
+++ math.js
@@ -0,0 +1,61 @@
+// global variables
+var operators = ["+", "-", "*", "/"];
+var number1, number2, operator;
+var score = 0;
+
+// start function
+function start() {
+  number1 = Math.floor(Math.random() * 50) + 1;
+  number2 = Math.floor(Math.random() * 20) + 1;
+  operator = operators[Math.floor(Math.random() * operators.length)];
+
+  document.getElementById("number1").innerHTML = number1;
+  document.getElementById("number2").innerHTML = number2;
+  document.getElementById("operator").innerHTML = operator;
+  document.getElementById("reset").style.visibility = "hidden";
+  document.getElementById("score").innerHTML = score;
+  reset();
+}
+
+// resetting the input and error message
+function reset() {
+  document.getElementById("answer").focus();
+  document.getElementById("answer").value = "";
+  document.getElementById("result").innerHTML = "";
+  return true;
+}
+
+// check answer
+function checkAnswer() {
+  var answer = document.getElementById("answer").value;
+  if (answer != "") {
+    var message = "";
+    var equation = eval(number1 + operator + number2);
+    if (isNaN(answer)) {
+      message =
+        "<span class='blue'>Hey! " + answer + " is not a valid number!</span>";
+    } else {
+      if (equation == answer) {
+        score += 10;
+        message = "<span class='green'>" + answer + " is correct!</span>";
+      } else {
+        score -= 10;
+        message = "<span class='red'>" + answer + " is incorrect!</span>";
+        document.getElementById("reset").style.visibility = "visible";
+      }
+    }
+    // display answer
+    document.getElementById("result").innerHTML = message;
+    document.getElementById("score").innerHTML = score;
+  }
+  return true;
+}
+
+// check for answer when enter is pressed
+document.getElementById("answer").addEventListener("keyup", function(e) {
+  // 13 is for enter key
+  if (e.which == 13 || e.keyCode == 13) {
+    checkAnswer();
+  }
+  return false;
+});
